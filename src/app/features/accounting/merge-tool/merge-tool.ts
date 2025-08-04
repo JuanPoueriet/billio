cff7ed990dc94511e99172909e467fae5149b163
Juan Poueriet <juanpoueriet00@gmail.com>
2025-08-04 00:43:59 -0400

    commit antes de empezar a agregar las funciones nuevas a chart of accounts

---
+++ src/app/features/accounting/merge-tool/merge-tool.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merge-tool',
+  imports: [],
+  templateUrl: './merge-tool.html',
+  styleUrl: './merge-tool.scss'
+})
+export class MergeTool {
+
+}
